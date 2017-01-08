36f16250874e8ed897612992d0657323711f8da0
Julian Winter <julian.winter@k-tc.de>
2017-01-08 22:00:10 +0100

    initial commit

---
+++ src/main/java/de/kaojo/cooking/clash/CoreServicesApplication.java
@@ -0,0 +1,12 @@
+package de.kaojo.cooking.clash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreServicesApplication.class, args);
+	}
+}
